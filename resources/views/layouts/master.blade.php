b86916a27e6c32feda6667d7ff56fc72cfa6494c
bluaeri <realnabluechan@gmail.com>
2022-07-04 00:10:09 +0700

    tugaslaporansurat

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -47,13 +47,13 @@
                             <a class="nav-link active" aria-current="page" href="/jadwal"><i class="bi bi-calendar3"></i>  Jadwal</a>
                         </li>
                         <li class="nav-item active">
-                            <a class="nav-link active" aria-current="page" href="/signup"><i class="bi bi-journal-text"></i>  Tugas dan Laporan</a>
+                            <a class="nav-link active" aria-current="page" href="/tugasLaporan"><i class="bi bi-journal-text"></i>  Tugas dan Laporan</a>
                         </li>
                         <li class="nav-item active">
                             <a class="nav-link active" aria-current="page" href="presensi.php"><i class="bi bi-clock-fill"></i>  Presensi</a>
                         </li>
                         <li class="nav-item active">
-                            <a class="nav-link active" aria-current="page" href="surat.php"><i class="bi bi-envelope-fill"></i>  Surat-surat</a>
+                            <a class="nav-link active" aria-current="page" href="/surat"><i class="bi bi-envelope-fill"></i>  Surat-surat</a>
                         </li>
                         <li class="nav-item active">
                             <a class="nav-link active" aria-current="page" href="keuangan.php"><i class="bi bi-graph-up-arrow"></i>  Keuangan</a>
